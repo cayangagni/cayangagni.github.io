1548f761e3ffe4cf7bbe48cef17e90b30334994b
cayangagni <152794919+cayangagni@users.noreply.github.com>
2023-12-03 21:53:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const wrapper document.querySelector(".wrapper");
 const question document.querySelector(".question");
 const gif document.querySelector(".gif"); const yesBtn = document.querySelector(".yes-btn");
-const noßtn document.querySelector(".no-btn");
-yesBtn.addEventListener("click", () {
+const noBtn document.querySelector(".no-btn");
+yesBtn.addEventListener("click", () => {
 question. InnerHTML = "Aaaaa, I like you too agnii sayangg";
-}); "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
-gif.src =
-noßtn.addEventListener("mouseover", () {
-const noßtnRect noßtn.getBoundingClientRect(); const maxx window.innerWidth noßtnRect.width;
+gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+});
+noBtn.addEventListener("mouseover", () => {
+const noBtnRect noBtn.getBoundingClientRect(); const maxx window.innerWidth noBtnRect.width;
 const maxy window.innerHeight noBtnRect.height;
 const randomX = Math.floor(Math.random() maxx); const randomy = Math.floor(Math.random() maxY);
-noßtn.style.left = randomX + "px";
+noBtn.style.left = randomX + "px";
 noBtn.style.top randomy + "px"; 
 });
