2f4c342ea500601980bad780f48242f287b4afa8
cayangagni <152794919+cayangagni@users.noreply.github.com>
2023-12-03 21:58:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 noBtn.addEventListener("mouseover", () => {
 const noBtnRect noBtn.getBoundingClientRect(); const maxx window.innerWidth noBtnRect.width;
 const maxy window.innerHeight noBtnRect.height;
-const randomX = Math.floor(Math.random() maxx); const randomy = Math.floor(Math.random() maxY);
-noBtn.style.left = randomX + "px";
+const randomX = Math.floor(Math.random() + maxx); const randomy = Math.floor(Math.random() + maxy);
+noBtn.style.left = randomx + "px";
 noBtn.style.top randomy + "px"; 
 });
